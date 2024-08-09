512d47eed3a1aab1ecb4984dafdf45e336efcd5d
Simra Waheed <simraaawaheed3@gmail.com>
2024-08-10 00:16:51 +0500

    Gulp workflow

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict"; console.log("HELLO");
+//# sourcemappingURL=script.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,26 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
-const fs = require('fs');
 const browsersync = require('browser-sync').create();
 // Use dart-sass for @use
 //sass.compiler = require('dart-sass');
-async function asyncAwaitTask() {
-    const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-    console.log(version);
-    await Promise.resolve('some result');
-  }
+
+    
 // Sass Task
-function scssTask(cb) {
-    return src('app/scss/styles.scss', { sourcemaps: true })
+async function scssTask() {
+    return src('app/scss/styles.scss', { sourcemaps: true, allowEmpty: true })
     .pipe(sass())
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest('dist', {sourcemaps: '.' }));
-    cb();   
+    
 }
 
 // Javascript Task
-function jsTask(cb) {
-    return src('app/js/scirpt.js', { sourcemaps: true })
+async function jsTask() {
+    return src('app/js/scirpt.js', { sourcemaps: true, allowEmpty: true })
     .pipe(babel({ presets: ['@babel/preset-env'] }))
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
-    cb();
 }
 
 // Browesersync
@@ -54,7 +49,7 @@ function browserSyncReLoad(cb) {
 }
 
 // Watch Task
-function watchTask() {
+async function watchTask() {
     watch('*.html', browserSyncReLoad);
     watch(
         ['app/scss/**/*.scss', 'app/**/*.js'],
@@ -63,6 +58,5 @@ function watchTask() {
 }
 
 // Default Gulp Task
-exports.default = asyncAwaitTask;
-exports.default = (scssTask, jsTask, browserSyncServe, watchTask);
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
 
